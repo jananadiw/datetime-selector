7f21eed959778e60f755cc1adb884ed237571fb0
Jananadi Wedagedara <jananadi92@gmail.com>
2023-05-10 18:58:11 +0900

    improvement: Move setup buttons to clock, remove unused methods and imports

--- src/DateTimePicker.tsx
+++ src/DateTimePicker.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Button } from './components/Button';
+import React, { useEffect, useState } from 'react';
 import { Calendar } from './components/Calendar';
 import { Clock } from './components/Clock';
 
@@ -8,23 +7,11 @@ import './styles/common/_button.scss';
 
 export default function DateTimePicker() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const setSelectedDate = () => {
-    console.log('selected date', currentDate);
-    return currentDate;
-  };
-  const initSelection = () => {
-    console.log('empty selected date');
-  };
+
   return (
     <div className='date-time-selector'>
       <Calendar value={currentDate} onChange={setCurrentDate} />
-      <Clock />
-      {/* <StartTimeInput />
-      <EndTimeInput /> */}
-      <div className='date-time-selector__actions'>
-        <Button className='button__set-up' text={'Set up'} onClick={setSelectedDate} />
-        <Button className='button__cancel' text={'Cancel'} onClick={initSelection} />
-      </div>
+      <Clock value={currentDate} onChange={setCurrentDate} />
     </div>
   );
 }
