04937ef3da100699532805b103914c5dd8c211ff
Jananadi Wedagedara <jananadi92@gmail.com>
2023-05-17 10:34:47 +0900

    chore: Prepare environment for testing

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { DateTimePicker } from './src/DateTimePicker';
+import { Button } from './src/components/Button';
+import { Calendar } from './src/components/Calendar';
+import { Clock } from './src/components/Clock';
+import { TimePicker } from './src/components/TimePicker';
+
+import './styles/layout.scss';
+import './styles/button.scss';
+
+export { DateTimePicker, Button, Calendar, Clock, TimePicker };
+export default DateTimePicker;
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { DateTimePicker } from './DateTimePicker';
-
-function App() {
-  return <DateTimePicker />;
-}
-
-export default App;
